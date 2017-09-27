f2c545987cb666cd5d2103eb6cca0c5ad1310253
Dostoievski Batista <dostoievskiab@gmail.com>
2017-09-26 22:41:09 -0300

    La vamos nos

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const porta = 3000 //Porta do listening
+const MongoClient = require('mongodb').MongoClient
+var db
+
+//Conexão com o DB e informações básicas sobre o funcionamento
+MongoClient.connect('mongodb://dbuser:Teste123@ds149324.mlab.com:49324/db-crud', (err,database) =>
+{
+  if (err) return console.log(err)
+  db = database
+  app.listen(porta, function() {
+    console.log('Listening na Porta: ' + porta);
+    console.log('Caminho: ' + __dirname);
+  })
+})
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
+
+
+app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray((err, result) =>{
+    if (err) return console.log(err)
+    res.render('index.ejs', {quotes: result})
+  })
+})
+
+
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) =>
+  {
+    if (err) return console.log(err)
+    res.redirect('/')
+  })
+})
